46ac349adaca731bfa50559793929cafc836a32a
JonasFortes12 <jonascforte@hotmail.com>
2023-10-31 13:59:13 -0300

    fix: adjuste  FT´ x axiss

--- low-pass-Filtering.py
+++ low-pass-Filtering.py
@@ -36,10 +36,10 @@ plot_graph(n, Xn, 'Sinal de entrada X[n]', 'Tempo', 'Amplitude')
 # Calcula o módulo da transformada de Fourier do sinal X[n]
 XnFT = np.abs(np.fft.fftshift(np.fft.fft(Xn)))
 
-# Calcula as frequências correspondentes às amostras
-freqs = np.fft.fftfreq(N)
+# Definir valores do eixo X
+x =  np.linspace(-np.pi, np.pi, N)
 
-plot_graph(freqs, XnFT, 'Módulo da FT do sinal X[n]', 'Frequência', 'Amplitude', color='black')
+plot_graph(x, XnFT, 'Módulo da FT do sinal X[n]', 'Frequência', 'Amplitude', color='black')
 
 #__________________________ Questão 03 _____________________________________
 
@@ -68,7 +68,17 @@ plot_graph(n, h_ideal, 'Resposta ao Impulso do Filtro Passa-Baixa FIR h[n]', 'Te
 # Calcula o módulo da resposta em frequência do sistema (FT da reposta ao impulso h[n])
 h_idealFT = np.abs(np.fft.fftshift(np.fft.fft(h_ideal)))
 
-# Calcula as frequências correspondentes às amostras
-freqs = np.fft.fftfreq(M)
+# Definir valores do eixo X
+x = np.linspace(-np.pi, np.pi, M)
 
-plot_graph(freqs, h_idealFT, 'Módulo da Resposta em Frequência do Sistema', 'Frequência', 'Amplitude', color='black')
+plot_graph(x, h_idealFT, 'Módulo da Resposta em Frequência do Sistema', 'Frequência', 'Amplitude', color='black')
+
+#__________________________ Questão 05 _____________________________________
+
+# Calcula a resposta em fase da da resposta em frequência do sistema
+phase_h_idealFT = np.angle(h_idealFT)
+
+plot_graph(x, phase_h_idealFT, 'Resposta em Fase do Sistema', 'Frequência', 'Amplitude', color='black')
+
+
+
